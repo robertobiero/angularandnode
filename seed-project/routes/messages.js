203a45a80e103128ad8bdd536ed227434af5f6bf
RXO1707 <robert_m_obiero@homedepot.com>
2017-06-22 08:26:35 -0400

    changes to message.js

--- seed-project/routes/messages.js
+++ seed-project/routes/messages.js
@@ -3,6 +3,28 @@ var router = express.Router();
 console.log("before the postermessage");
 var Message = require('../models/message');
 
+
+
+router.get('/', function (req, res, next) {
+  Message.find().
+          exec(function(err,messages){
+               if(err){
+                  console.log("in the loggerrrrr");
+                return res.status(500).json({
+                title: 'an error',
+                error: err
+               });
+
+            }
+            res.status(200).json({
+            message: 'Success',
+            obj: messages
+        });
+
+     });
+
+});
+
 router.post('/', function (req, res, next) {
     console.log("before the poster");
     var message = new Message({
